62dbbe47b8dd2202d4e85aaea7f008aeaa2dfe6d
agustin <agustin.piqueres@gmail.com>
2021-10-08 15:52:42 +0200

    doc: removed comment on top of module

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-r"""FastAPI app service.
-
-Functionalities to implement:
-- Endpoint to read a report from current workind directory
-    Expect a .reducto_report.json in the folder root
-- Parse the file to obtain the data
-- Generate request for shields.io (personalized by one of the variables initially):
-    i.e. https://img.shields.io/badge/python-hey-green
-- Obtain the image returned by the server
-- Serve the image
-
-TODO:
-    - Idea to serve the images:
-    https://stackoverflow.com/questions/55873174/how-do-i-return-an-image-in-fastapi/67497103#67497103
-
-TODO:
-    RedirectResponse
-    - Parse file
-    - Generate url
-
-    - To obtain a varaible from json file.
-    https://fastapi.tiangolo.com/tutorial/query-params/
-
+"""FastAPI app service.
 
 Python colors:
 https://www.schemecolor.com/python-logo-colors.php
-
 
 """
 
@@ -36,6 +13,7 @@ from jsonschema import validate, ValidationError
 import aiofiles
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi import File, UploadFile
 
 
 BASE_URL = r'https://img.shields.io/badge/<LABEL>-<MESSAGE>-green?logo=python&color=ffd43b'
@@ -114,25 +92,6 @@ async def read_report(file_path: str) -> Dict:
         # If the validation passed, extract inner report:
         package_name: str = list(report.keys())[0]
         return report[package_name]
-
-
-# @app.get("/report/{file_path:path}")
-# async def read_file(file_path: str):
-#     """Function to read a file (expected to be at the same level of this very file.
-#
-#     TODO:
-#         Create a sample file for testing purposes.
-#
-#     Parameters
-#     ----------
-#     file_path : str
-#         Name of the file. As expected in relative, should be the only info necessary.
-#
-#     Examples
-#     --------
-#     http://127.0.0.1:8000/report/reducto_report.json
-#     """
-#     return await read_report(file_path)
 
 
 def generate_url(variable: str, report: Dict) -> str:
